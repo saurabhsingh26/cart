837ea0b6fb860b6a70bf15f2e07daa991d6ed1c3
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2022-08-29 22:49:51 +0530

    Add Navbar and change CartItem from class to function based

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,44 +1,42 @@
 import React from 'react';
 
-class CartItem extends React.Component {
+const CartItem = (props) => {
     
-    render() {
-        // console.log(this.props.product);
-        const {price, title, qty } = this.props.product
+    const {price, title, qty } = props.product // We don't need to the this keyword we don't have access to it because this is not a class anymore.
 
-        return (
-            <div className='cart-item'>
-                <div className='left-block'>
-                    <img style={styles.image} alt=""/>
-                </div>
-                <div className='right-block'>
-                    <div style={{fontSize: 25}}>{title}</div>
-                    <div style={{color: '#777'}}>Rs. {price}</div>
-                    <div style={{color: '#777'}}>Qty: {qty}</div>
-                    <div className='cart-item-actions'>
-                        <img 
-                            className='action-icons' 
-                            alt="increase" 
-                            src="https://cdn-icons-png.flaticon.com/512/7168/7168754.png"
-                            onClick={() => this.props.onIncreaseQuantity(this.props.product)}
-                        />
-                        <img 
-                            className='action-icons' 
-                            alt="decrease" 
-                            src="https://cdn-icons-png.flaticon.com/512/6461/6461294.png" 
-                            onClick={() => this.props.onDecreaseQuantity(this.props.product)}
-                        />
-                        <img 
-                            className='action-icons' 
-                            alt="delete" 
-                            src="https://cdn-icons-png.flaticon.com/512/3221/3221897.png" 
-                            onClick={() => this.props.onDeleteProduct(this.props.product.id)}
-                        />
-                    </div>
+    return (
+        <div className='cart-item'>
+            <div className='left-block'>
+                <img style={styles.image} alt=""/>
+            </div>
+            <div className='right-block'>
+                <div style={{fontSize: 25}}>{title}</div>
+                <div style={{color: '#777'}}>Rs. {price}</div>
+                <div style={{color: '#777'}}>Qty: {qty}</div>
+                <div className='cart-item-actions'>
+                    <img 
+                        className='action-icons' 
+                        alt="increase" 
+                        src="https://cdn-icons-png.flaticon.com/512/7168/7168754.png"
+                        onClick={() => props.onIncreaseQuantity(props.product)}
+                    />
+                    <img 
+                        className='action-icons' 
+                        alt="decrease" 
+                        src="https://cdn-icons-png.flaticon.com/512/6461/6461294.png" 
+                        onClick={() => props.onDecreaseQuantity(props.product)}
+                    />
+                    <img 
+                        className='action-icons' 
+                        alt="delete" 
+                        src="https://cdn-icons-png.flaticon.com/512/3221/3221897.png" 
+                        onClick={() => props.onDeleteProduct(props.product.id)}
+                    />
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+    
 }
 
 // Style our elements using objects
